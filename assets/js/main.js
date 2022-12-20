c7f7b58fa2074edaa455930492e402e015819e52
lucasFigueira1 <lucasfigueira137@gmail.com>
2022-12-20 16:49:05 -0300

    Segunda entrega coder

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,53 @@
-// Primer entrega: Prompt, if (condicional), funtions, for (bucle), class y objetos
+// Segunda entrega: Prompt, if (condicional), funtions, for (bucle), class y objetos, Arrays, DOM
 
-// Choose theme when joining (To Change)
-// let websiteTheme = prompt("Choose Theme: 1(Dark) or 2(Light)");
-// let b = document.body;
+// Choose theme when joining (*To Change*)
+let websiteTheme = prompt("Choose Initial Theme: 1(Dark) or 2(Light)");
 
-// if (websiteTheme === "1") {
-//   b.style.backgroundColor = "hsl(19, 16%, 15%)";
-//   b.style.color = "hsl(19, 100%, 96%)";
-// } else if (websiteTheme === "2") {
-//   b.style.backgroundColor = "hsl(19, 100%, 96%)";
-// } else {
-//   b.style.backgroundColor = "hsl(19, 100%, 96%)";
-// }
+if (websiteTheme === "1") {
+  document.body.classList.toggle("dark-theme");
+} else if (websiteTheme === "2") {
+  document.body.style.backgroundColor = "hsl(19, 100%, 96%)";
+} else {
+  document.body.classList.toggle("dark-theme");
+}
+
+/*===== Uso de arrays (*To change*) =====*/
+let showCatalog = prompt(
+  "If you want to take a look at the catalog, write: Mates or Bombillas or Materas"
+);
+let Catalog = [
+  {
+    name: "Mates",
+    products: [
+      { pname: "Mate Camionero", price: 2900 },
+      { pname: "Mate Torpedo", price: 2400 },
+      { pname: "Mate Imperial", price: 3400 },
+    ],
+  },
+  {
+    name: "Bombillas",
+    products: [
+      { pname: "Acero", price: 1000 },
+      { pname: "Alpaca", price: 1200 },
+      { pname: "Bronce", price: 800 },
+    ],
+  },
+  {
+    name: "Materas",
+    products: [
+      { pname: "Mochila Matera + Porta Notebook", price: 20000 },
+      { pname: "Canasta de cuero", price: 7000 },
+      { pname: "Bolso Matero", price: 7000 },
+    ],
+  },
+];
+
+function filterCatalog() {
+  let keyword = showCatalog;
+  const otroFiltrado = Catalog.filter((item) => item.name.includes(keyword));
+  alert(JSON.stringify(otroFiltrado));
+}
+filterCatalog();
 
 // Burger Menú
 const burgerMenu = document.querySelector(".bars-menu");
@@ -44,7 +80,7 @@ function displayNavMenu() {
       line2.classList.remove("activeline2__bars-menu");
       line3.classList.remove("activeline3__bars-menu");
 
-      document.body.classList.remove("stop-scrolling")
+      document.body.classList.remove("stop-scrolling");
     });
   }
 }
@@ -59,18 +95,27 @@ let calcScrollValue = () => {
   let pos = document.documentElement.scrollTop;
 
   if (pos > 300) {
-    arrow.style.display = "flex"
+    arrow.style.display = "flex";
   } else {
-    arrow.style.display = "none"
+    arrow.style.display = "none";
   }
 
-  arrow.addEventListener("click", ()=> {
+  arrow.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
-  })
-}
+  });
+};
 
 window.onscroll = calcScrollValue;
 window.onload = calcScrollValue;
+
+/*====== DARK LIGHT THEME ======*/
+const themeButton = document.getElementById("theme-button");
+const iconTheme = "ri-sun-line";
+
+themeButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+  themeButton.classList.toggle(iconTheme);
+});
 
 // Shopping Cart
 // When confirmed the buy, is going to add the amount of the price plus iva
@@ -87,4 +132,3 @@ class ProductAddedToCart {
 
 let prod1 = new ProductAddedToCart("Mate Camionero", "Calabaza", 3220);
 prod1.productPricePlusIva();
-
